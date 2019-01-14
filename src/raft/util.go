12efbb77f1f8a4507a4fc58bc70d73edb4d12cb8
Saurabh Sharan <thesaurabhsharan@gmail.com>
2019-01-14 00:47:12 -0800

    Update util.go

--- src/raft/util.go
+++ src/raft/util.go
@@ -6,8 +6,20 @@ import "log"
 const Debug = 0
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug > 2 {
+		log.Printf(format, a...)
+	}
+	return
+}
+
+func IPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 1 {
 		log.Printf(format, a...)
 	}
+	return
+}
+
+func EPrintf(format string, a ...interface{}) (n int, err error) {
+	log.Printf(format, a...)
 	return
 }
